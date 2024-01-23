0b7cf11fa398e05cb3031f714633a24e0f1f0492
aditya <adityanarayanrajan@gmail.com>
2024-01-23 12:02:20 +0530

    JoinRoom Page

---
+++ src/sections/join/JoinRoomForm.tsx
@@ -0,0 +1,167 @@
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useContext, useMemo, useState } from "react";
+import { AppContext } from "@/context/AppContext";
+import { useRouter } from "next/navigation";
+import { joinRoomDataObject } from "@/types";
+import { useToast } from "@/components/ui/use-toast";
+import { Button } from "@/components/ui/button";
+const FruitName = [
+  "Aam",
+  "Khira",
+  "Akhrot",
+  "Badam",
+  "Kaju",
+  "Santra",
+  "Mausambi",
+  "Gagar",
+  "Kela",
+  "Anar",
+];
+
+export const JoinRoomForm = () => {
+  const defaultName = useMemo(() => {
+    return FruitName[Math.floor(Math.random() * 10)];
+  }, []);
+
+  const router = useRouter();
+  const { toast } = useToast();
+  const { socket, dispatch } = useContext(AppContext);
+
+  const [showPasswordField, setShowPasswordField] = useState(false);
+  const [errors, setErrors] = useState({ password: "", room: "" });
+
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validateData = (
+    name: string,
+    room: string,
+    password: string
+  ): joinRoomDataObject => {
+    let data: any = {};
+    try {
+      // name
+      if (name) name = name.trim();
+      data.name = name || defaultName;
+
+      // room
+      room = room.trim();
+      if (!room) throw { field: "room", message: "Room ID is required" };
+      data.roomId = room;
+
+      // password
+      if (showPasswordField) {
+        password = password.trim();
+        if (password.length == 0)
+          throw { field: "password", message: "Password is required" };
+        data.password = password;
+      }
+
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      let data = validateData(name, room, password);
+      let res = await socket.emitWithAck("join-room", data);
+      if (res.status == "ERROR") throw res;
+      dispatch({
+        type: "UPDATE_ROOM",
+        roomId: room,
+        name: data.name,
+        timeOut: Number(res.timeOut),
+      });
+      router.push("room");
+    } catch (error: any) {
+      if (error.type == "INVALID_ROOMID") {
+        setErrors((p) => ({ ...p, room: error.message }));
+      } else if (error.type == "PASSWORD_REQUIRED") {
+        setShowPasswordField(true);
+        toast({
+          title: "Password protected",
+          description: "This room is password protected",
+        });
+      } else if (error.type == "INVALID_PASSWORD") {
+        setErrors((p) => ({ ...p, password: error.message }));
+        toast({
+          title: "Invalid Password",
+          description: "You have entered invalid password",
+        });
+      } else if (error.field) {
+        setErrors((p) => ({ ...p, [error.field]: error.message }));
+      } else {
+        toast({
+          description: "Something Went Wrong !",
+          variant: "destructive",
+        });
+      }
+    }
+  };
+  return (
+    <>
+      {/* Name */}
+      <div className="flex flex-col space-y-1.5">
+        <Label htmlFor="name">Name</Label>
+        <Input
+          id="name"
+          placeholder={defaultName}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+
+      {/* Room */}
+      <div className="flex flex-col space-y-1.5">
+        <Label htmlFor="name">Room ID</Label>
+        <Input
+          id="name"
+          placeholder={"Enter Room ID"}
+          value={room}
+          onChange={(e) => {
+            errors.room && setErrors((p) => ({ ...p, room: "" }));
+            setRoom(e.target.value);
+          }}
+          className={!!errors.room ? "border-red-500" : ""}
+          disabled={showPasswordField}
+        />
+        {!!errors.room && (
+          <p className="text-red-500 text-xs font-semibold">{errors.room}</p>
+        )}
+      </div>
+
+      {/* Password */}
+      {showPasswordField && (
+        <div className="flex flex-col space-y-1.5">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            placeholder="Enter Password"
+            name="password"
+            type="password"
+            className={!!errors.password ? "border-red-500" : ""}
+            value={password}
+            onChange={(e) => {
+              errors.password && setErrors((p) => ({ ...p, password: "" }));
+              setPassword(e.target.value);
+            }}
+          />
+          {!!errors.password && (
+            <p className="text-red-500 text-xs font-semibold">
+              {errors.password}
+            </p>
+          )}
+        </div>
+      )}
+
+      <Button className="w-full" onClick={handleSubmit}>
+        Join Room
+      </Button>
+    </>
+  );
+};
